45f0aa3e7094486557b91ea90fddb125b067a2cd
Gonzalo Fernandez <gfr@tid.es>
2013-10-30 13:35:02 +0100

    Adding the Class closed for modification

---
+++ src/main/java/com/solid/openclosed/okStrategy/OkOpenClosed.java
@@ -0,0 +1,10 @@
+package com.solid.openclosed.okStrategy;
+
+
+public class OkOpenClosed
+{
+	public static final String property2String(IOpenClosedProperty property)
+	{
+		return property.toString();
+	}
+}
