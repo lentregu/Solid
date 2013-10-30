362164f1f5e2a432d566f30d714045fce00488c3
Gonzalo Fernandez <gfr@tid.es>
2013-10-30 12:12:52 +0100

    Wrong Open-Closed Principle

---
+++ src/main/java/com/solid/openclosed/WrongOpenClosed.java
@@ -0,0 +1,27 @@
+package com.solid.openclosed;
+
+public class WrongOpenClosed
+{
+		
+	public static final String property2String(Object property) {
+		if(property == null) {
+			return "";
+		} else if (property instanceof String) {
+			return (String)property;
+		} else if (property instanceof String[]) {
+			String[] array = (String[])property;
+			if(array.length == 1) {
+				return array[0];
+			} else {
+				StringBuilder result = new StringBuilder();
+				for (int i = 0; i < array.length; i++) {
+					result.append(array[i]);
+					result.append(' ');
+				}
+				return result.toString();
+			}
+		} else {
+			return property.toString();
+		}
+	}
+}
